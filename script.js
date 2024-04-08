1f32433027d56aba9b0e35f87f507cd9c35bf811
meghna7838 <meghnatandon7838@gmail.com>
2024-04-08 18:35:08 +0530

    HTML Part

---
+++ script.js
@@ -0,0 +1,10 @@
+const emojiDetails = [
+    {description: "Smiling face with sunglasses", emoji:'😎' },
+    { description: "Thumbs up", emoji: "👍" },
+    { description: "Heart eyes", emoji: "😍" },
+    { description: "Crying face", emoji: "😢" },
+    { description: "Party popper", emoji: "🎉" },
+    // Add more emoji descriptions here
+  ];
+
+ 
